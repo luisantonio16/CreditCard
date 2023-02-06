8b5e51d1bcbc724df03cc8377c621a95d88f45d6
luisantonio16 <luis16.16m@gmail.com>
2023-02-06 12:14:49 -0400

    updte

--- javaScript/main.js
+++ javaScript/main.js
@@ -8,6 +8,25 @@ const inpuntName = document.getElementById('inputnombre');
 const pfirma = document.querySelector('.p-firma');
 const pccv = document.querySelector('.p-ccv');
 const inpuntCCV = document.getElementById('inputCcv');
+const selectMes = document.getElementById('select-mes');
+const selectDia = document.getElementById('select-año');
+const mes = document.querySelector('.mes');
+const year = document.querySelector(".year")
+const btnEnviar = document.querySelector('.btn-enviar')
+const body = document.querySelector(".hide");
+const img = document.querySelector(".img-hide");
+
+
+
+
+window.addEventListener("load",()=>{
+    llenarSelectMes();
+    llenarSelectDia();
+
+})
+ 
+
+   
 
 
 cardAfter.addEventListener('click', ()=>{
@@ -20,21 +39,95 @@ cardBefore.addEventListener('click', ()=>{
 
 })
 
-inpuntNumero.addEventListener('input' ,()=>{
-    pNumero.textContent = inpuntNumero.value
+inpuntNumero.addEventListener('keyup' ,(e)=>{
+   let value = e.target.value
+   inpuntNumero.value = value.replace(/\s/g, '')
+   .replace(/\D/g,'')
+   .replace(/([0-9]{4})/g, '$1 ').trim();
+    mostrar();
+    pNumero.textContent = value
+    if(value ===""){
+       pNumero.textContent = "1001 2002 3003 4004"
 
+    }
+ 
 });
 
-inpuntName.addEventListener('input',()=>{
-    pName.textContent = inpuntName.value
-    pfirma.textContent = inpuntName.value
+inpuntName.addEventListener('keyup',(e)=>{
+    let value = e.target.value
+    inpuntName.value = value.replace(/[0-9]/g, '')
+    mostrar();
+    pName.textContent = value
+    pfirma.textContent = value
+    if(value ===""){
+        pName.textContent ="Luis Melenciano"
+        pfirma.textContent = "Luis Melenciano"
+
+    }
+ 
 })
 
 inpuntCCV.addEventListener('input',(e)=>{
-    console.log(e.target);
-     carContainer.classList.add("flipped")
-     pccv.textContent = inpuntCCV.value
+    if(carContainer.classList.contains("flipped")){
+        carContainer.classList.add("flipped");
+        pccv.textContent = inpuntCCV.value;
+        
+    }
+    else{
+        carContainer.classList.add("flipped")
+        pccv.textContent = inpuntCCV.value;
 
-    
+    }
+})
+
+selectMes.addEventListener('change',(e)=>{
+    mes.textContent = e.target.value
+    mostrar();
+})
+
+selectDia.addEventListener('change' , (e)=>{
+    year.textContent = e.target.value.slice(2);
+    mostrar();
 
 })
+
+function llenarSelectMes() {
+    for (let i = 0; i <=12; i++) {
+        let opcion = document.createElement('option');
+        opcion.value = i
+        opcion.innerHTML = i
+        selectMes.appendChild(opcion);    
+    }  
+}
+
+function llenarSelectDia() {
+    const years = new Date().getFullYear();
+    for (let i = years; i <=years + 6; i++) {
+        let opcion = document.createElement('option');
+        opcion.value = i
+        opcion.innerHTML = i
+        selectDia.appendChild(opcion);    
+    }  
+}
+
+function mostrar() {
+    if(carContainer.classList.contains("flipped")){
+        carContainer.classList.remove("flipped");
+       
+  }   
+}
+
+
+btnEnviar.addEventListener('click',()=>{
+     body.classList.add("show")
+     carContainer.classList.add("rotate")
+})
+
+img.addEventListener('click',()=>{
+       body.classList.remove("show")
+       carContainer.classList.remove("rotate")
+})
+
+
+
+
